80f2a4ec7db9a411798081bd285a4091f74bde70
HenokTsegaye <maxhenock@gmail.com>
2021-09-11 13:32:47 +0300

    feat: separating the top-down approch and memoization based on navie

--- fibonanci-series/memoization.js
+++ fibonanci-series/memoization.js
@@ -1,43 +1,32 @@
 const chalk = require("chalk");
 
 /**
- * a memoization approach for making a fibonacci call
- * time-complexity - 0(n)
+ * a naive approach for making a fibonacci call
+ * time-complexity - 0(2)^n
  * space-complexity - 0(n)
  * @param {number} n thenth fibonacci series
  * @returns calculated fibonacci series
  */
-function fibonacciCalculator(n, obj) {
+function fibonacciCalculator(n, arr = {}) {
   if (n < 0) return false; //  constant time calculation , c
 
-  if (!obj[n]) {
-    var i = 3;
-    while (i <= n) {
-      obj[i] =
-        fibonacciCalculator(i - 2, obj) + fibonacciCalculator(i - 1, obj);
-      i++;
-    }
-    return obj[n];
-  }
-
-  return obj[n]; //returns the memoized value if it already exists
+  if (n === 1 || n === 2) return 1;
+  if (!arr[n])
+    arr[n] = fibonacciCalculator(n - 1, arr) + fibonacciCalculator(n - 2, arr);
+  return arr[n];
 }
-
-//this needs to be global as it has to be refereed everywhere and its like a global data store for each function call
-var obj = { 1: 1, 2: 1 };
-
 
 console.log(
   chalk.magenta(
     `4^th fibonacci series >  ${chalk.green.bold.italic(
-      fibonacciCalculator(4, obj)
+      fibonacciCalculator(4)
     )} `
   )
 );
 console.log(
   chalk.magenta(
-    `122^th fibonacci series >  ${chalk.green.bold.italic(
-      fibonacciCalculator(42, obj)
+    `42^th fibonacci series >  ${chalk.green.bold.italic(
+      fibonacciCalculator(42)
     )} `
   )
 );
---
+++ fibonanci-series/top-down-memoization.js
@@ -0,0 +1,43 @@
+const chalk = require("chalk");
+
+/**
+ * a memoization approach for making a fibonacci call
+ * time-complexity - 0(n)
+ * space-complexity - 0(n)
+ * @param {number} n thenth fibonacci series
+ * @returns calculated fibonacci series
+ */
+function fibonacciCalculator(n, obj) {
+  if (n < 0) return false; //  constant time calculation , c
+
+  if (!obj[n]) {
+    var i = 3;
+    while (i <= n) {
+      obj[i] =
+        fibonacciCalculator(i - 2, obj) + fibonacciCalculator(i - 1, obj);
+      i++;
+    }
+    return obj[n];
+  }
+
+  return obj[n]; //returns the memoized value if it already exists
+}
+
+//this needs to be global as it has to be refereed everywhere and its like a global data store for each function call
+var obj = { 1: 1, 2: 1 };
+
+
+console.log(
+  chalk.magenta(
+    `4^th fibonacci series >  ${chalk.green.bold.italic(
+      fibonacciCalculator(4, obj)
+    )} `
+  )
+);
+console.log(
+  chalk.magenta(
+    `42^th fibonacci series >  ${chalk.green.bold.italic(
+      fibonacciCalculator(42, obj)
+    )} `
+  )
+);
