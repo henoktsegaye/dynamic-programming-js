a9eca9da98ceedfdd99fd62d31cc2c91d60cfdfc
HenokTsegaye <maxhenock@gmail.com>
2021-09-14 23:31:43 +0300

    feat: update the knapsack problem with naive approch

--- knapsack-problem/naive.js
+++ knapsack-problem/naive.js
@@ -5,8 +5,8 @@
  */
 /**
  * this algorithm works for only 3 levels
- * @param {Object} values 
- * @param {Number} capacity 
+ * @param {Object} values
+ * @param {Number} capacity
  * @returns returns the value
  */
 function findKnapSack(values, capacity) {
@@ -49,13 +49,31 @@ function findKnapSack(values, capacity) {
   return maxValue;
 }
 
-
-function knapsackImproved (values, capacity) {
-    const { weights, prices} = values;
-
+/**
+ * the way to solve this problem is just by comparing
+ * @param {*} values
+ * @param {*} capacity
+ */
+function knapsackImproved(n, values, capacity) {
+  console.log("called, n = %s, capacity = %s", n, capacity);
+  var result = 0;
+  const { weights, prices } = values;
+//  if (n ==0) result = 0;
+   if (weights[n] <= capacity ) {
+    result = Math.max(
+      knapsackImproved(n - 1, values, capacity ),
+      prices[n] + knapsackImproved(n - 1, values, capacity- weights[n])
+    );
+  } else if (weights[n] > capacity ) {
+    result = knapsackImproved(n - 1, values, capacity);
+  }
+  return result;
 }
 
+// console.log(
+//   findKnapSack({ weights: [ 2,3,4,5 ], prices: [1,2,5,6 ] }, 14)
+// );
 
 console.log(
-  findKnapSack({ weights: [ 2,3,4,5 ], prices: [1,2,5,6 ] }, 14)
+  knapsackImproved(3, { weights: [2, 3, 4, 5], prices: [1, 2, 5, 6] }, 14)
 );
